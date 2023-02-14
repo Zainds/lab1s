116b553b22ab9e1a1d0526af6270ac3f2ab7060b
Zainds <beat1097@mail.ru>
2023-02-14 08:33:59 +0700

    dia

--- src/Main2.java
+++ src/Main2.java
@@ -15,15 +15,21 @@ public class Main2 {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Enter argument"); x = in.nextDouble();
-        System.out.println("Enter accurecy"); accuracy = in.nextDouble();
+        //System.out.println("Enter accurecy"); accuracy = in.nextDouble();
         result = 0;
-        for (int i = 0; i < accuracy; i++){
-            result += (Math.pow(-1, i) * Math.pow(x, i)) / factorial(i);
+        double current = 1;
+        int i = 0;
+        while (Math.abs(current) > 1e-15) {
+            current = (Math.pow(-1, i) * Math.pow(x, i)) / factorial(i);
+            result += current;
+            i++;
         }
-
         System.out.println(result);
         System.out.println(Math.pow(Math.E, -x));
 
     }
 }
-//
+
+
+/*
+        }*/
